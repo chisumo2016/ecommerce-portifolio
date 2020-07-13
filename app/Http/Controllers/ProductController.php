9f4db29d2bafad53c70606becef80381ca57af01
chisumo <bchisumo74@yahoo.com>
2020-07-13 16:16:09 +0100

    Displaying the data to the view from productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,6 @@ class ProductController extends Controller
     public  function  index()
     {
         $products = Product::all();
-        dd($products);
 
         return view('products.index');
     }
@@ -28,9 +27,12 @@ class ProductController extends Controller
 
     public  function show($product)
     {
-          $product =Product::findOrFail($product);
-          dd($product);
-        return view('products.show');
+        $product =Product::findOrFail($product);
+
+        return view('products.show')->with([
+            'product' => $product,
+
+        ]);
     }
 
     public  function  edit($product)
