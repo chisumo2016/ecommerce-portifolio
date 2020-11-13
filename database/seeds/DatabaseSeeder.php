6cb59b1e8654dfd213b9a657f058cea0c61b35cb
chisumo <bchisumo74@yahoo.com>
2020-11-13 12:09:09 +0000

    Inserting simple model relationships within Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Order;
+use App\Payment;
 use App\Product;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +16,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $products = factory(Product::class, 50)->create();
+        $products   = factory(Product::class, 50)->create();
+        $users      = factory(User::class, 20)->create(); //$users = User::factory(20)->create();
+
+        $orders     = factory(Order::class, 10) //$orders = Order::factory(10)
+                    ->make()
+                    ->each(function ($order) use ($users){
+                        //associate oreder to one user
+                        $order->customer_id  = $users->random()->id;
+                        $order->save();
+
+          //assocaite new payment - one payment with each
+
+          $payment = factory(Payment::class)->make();     // $payment = Payment::factory()->make();
+                $order->payment()->save($payment);
+                //$payment->order_id  = $order->id;
+                //$payment->save();
+
+
+            }) ;
         //factory(App\Product::class, 50)->create();
     }
 }
