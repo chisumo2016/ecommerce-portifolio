0f768a0b92b4f75c5e4f6d555dd92b104d1389ae
chisumo <bchisumo74@yahoo.com>
2020-11-24 14:20:40 +0000

    Handling the  files image in user profile by detaching and attaching a new image

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,6 +43,14 @@ return [
 
     'disks' => [
 
+
+        'images' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/images'),
+            'url' => env('APP_URL').'/image',
+            'visibility' => 'public',
+        ],
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -80,6 +88,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('images') => storage_path('app/public/images'),
     ],
 
 ];
